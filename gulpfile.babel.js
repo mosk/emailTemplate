924aca6c2d26fa92239c5ede21e8a065b76113a8
Mihail <miiixsc@gmail.com>
2019-01-25 10:44:45 +0300

    *

---
+++ gulpfile.babel.js
@@ -0,0 +1,42 @@
+import gulp from 'gulp';
+import browserSync from 'browser-sync';
+import pug from 'gulp-pug';
+// import inlineCss from 'gulp-inline-css';
+import htmlbeautify from 'gulp-html-beautify';
+
+const server = browserSync.create();
+
+const reload = () => {
+	server.reload();
+	console.log(`Перезагрузка`);
+};
+
+const serve = () => {
+	server.init({
+		server: {
+			baseDir: `build/`
+		}
+	});
+
+	gulp.watch(`src/**/*.*`).on(`change`, watch);
+};
+
+const watch = () => {
+	gulp.watch(`src/**/*.*`, gulp.series(html, reload));
+};
+
+export const html = () => {
+	return gulp.src(`src/pug/index.pug`)
+		.pipe(pug())
+		.pipe(htmlbeautify({
+			"indent_size": 4,
+			"indent_char": ` `,
+			"unformatted": []
+		}))
+		.pipe(gulp.dest(`build/`));
+};
+
+
+const dev = gulp.parallel(html, serve);
+
+export default dev;
